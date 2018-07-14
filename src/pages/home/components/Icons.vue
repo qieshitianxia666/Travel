1cd03dd19f4a780803eaed6ca66d74bf9586713f
lkp <164685052@qq.com>
2018-07-14 20:35:00 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="icons">
+      <swiper>
+        <swiper-slide v-for="(page,index) of pages" :key="index">
+          <div class="icon"
+               v-for="item of page"
+               :key="item.id"
+          >
+            <div class="icon-img">
+              <img class="icon-imgcontent" :src="item.imgUrl" alt="">
+            </div>
+
+            <p class="icon-desc">{{item.desc}}</p>
+          </div>
+        </swiper-slide>
+      </swiper>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeIcons",
+        data(){
+          return {
+            iconList:[{
+              id:'0001',
+              imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+              desc:'景点门票数据库设计贷款纠纷'
+
+            },{
+              id:'0002',
+              imgUrl:'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+              desc:'滑雪季'
+            },{
+              id:'0003',
+              imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/6a/45f595250c73d102.png',
+              desc:'泡温泉'
+            },{
+              id:'0004',
+              imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+              desc:'动植园'
+            },{
+              id:'0005',
+              imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+              desc:'景点门票'
+
+            },{
+              id:'0006',
+              imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+              desc:'滑雪季'
+            },{
+              id:'0007',
+              imgUrl:'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+              desc:'泡温泉'
+            },{
+              id:'0008',
+              imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/54/35899492b1302802.png',
+              desc:'动植园'
+            },{
+              id:'0009',
+              imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/54/35899492b1302802.png',
+              desc:'随意游'
+            }]
+          }
+        },
+      computed:{
+          pages (){
+            const pages = []
+            this.iconList.forEach((item,index)=>{
+              const page = Math.floor(index/8)
+              if(!pages[page]){
+                pages[page] = []
+              }
+              pages[page].push(item)
+            })
+            return pages
+
+          }
+      }
+    }
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  @import "~styles/mixins.styl"
+    .icons >>> .swiper-container
+      height 0
+      padding-bottom 50%
+    .icon
+      position relative
+      overflow hidden
+      float left
+      width 25%
+      height 0
+      padding-bottom 25%
+      .icon-img
+        position absolute
+        top 0
+        left 0
+        right 0
+        bottom .44rem
+        padding .1rem
+        .icon-imgcontent
+          height 100%
+          display block
+          margin 0 auto
+      .icon-desc
+        position absolute
+        text-align center
+        left 0
+        right 0
+        bottom 0
+        height .44rem
+        line-height .44rem
+        color $darkTextColor
+        ellipsis()
+</style>
