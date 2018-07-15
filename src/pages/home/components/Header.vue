12014353ee82dbc0e577eea57c7cdd1fd267aa20
lkp <164685052@qq.com>
2018-07-15 16:29:20 +0800

    city search

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,57 +1,60 @@
 <template>
-    <div class="header">
-      <div class="header-left">
-        <div class="iconfont back-icon">&#xe624;</div>
-      </div>
-      <div class="header-input">
-        <span class="iconfont">&#xe632;</span>
-        输入城市/景点/游玩主题
-      </div>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <router-link to="/city">
       <div class="header-right">
         {{this.city}}
         <span class="iconfont arrow-icon">&#xe64a;</span>
       </div>
-    </div>
+    </router-link>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "HomeHeader",
-        props:{
-          city:String
-        }
+  export default {
+    name: "HomeHeader",
+    props: {
+      city: String
     }
+  }
 </script>
 
 <style lang="stylus" scoped>
   @import "~styles/varibles.styl"
-    .header
-      display flex
-      line-height .86rem
-      color #fff
-      background $bgcolor
-      .header-left
-        width .64rem
-        float left
-        .back-icon
-          text-align center
-          font-size .4rem
-      .header-input
-        flex 1
-        height .64rem
-        line-height .64rem
-        margin .12rem .2rem
-        padding-left .2rem
-        background #fff
-        border-radius .2rem
-        color #ccc
-
-      .header-right
-        width 1.24rem
-        float right
+  .header
+    display flex
+    line-height .86rem
+    color #fff
+    background $bgcolor
+    .header-left
+      width .64rem
+      float left
+      .back-icon
         text-align center
-        .arrow-icon
+        font-size .4rem
+    .header-input
+      flex 1
+      height .64rem
+      line-height .64rem
+      margin .12rem .2rem
+      padding-left .2rem
+      background #fff
+      border-radius .2rem
+      color #ccc
 
-          font-size .24rem
+    .header-right
+      width 1.24rem
+      float right
+      text-align center
+      color #fff
+      .arrow-icon
+
+        font-size .24rem
 
 </style>
